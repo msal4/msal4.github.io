51139428dbecae4013d5620390ab9966e2d581ae
Mohammed Salman <msal4@outlook.com>
2017-09-17 20:51:04 +0300

    lol

--- pixel-char-phaser/sketch.js
+++ pixel-char-phaser/sketch.js
@@ -1,10 +1,38 @@
 var GameState = {
     preload: function() {
         this.load.image("background", "assets/images/yaseen01.jpg");
-        this.load.image("char01", "assets/images/char01.png");
-        this.load.image("char02", "assets/images/char02.png");
-        this.load.image("char03", "assets/images/char03.png");
-        this.load.image("char04", "assets/images/char04.png");
+        this.load.spritesheet(
+            "char01",
+            "assets/images/spritesheet01.png",
+            256,
+            256,
+            6
+        );
+        this.load.spritesheet(
+            "char02",
+            "assets/images/spritesheet01.png",
+            256,
+            256,
+            6
+        );
+        this.load.spritesheet(
+            "char03",
+            "assets/images/spritesheet01.png",
+            256,
+            256,
+            6
+        );
+        this.load.spritesheet(
+            "char04",
+            "assets/images/spritesheet01.png",
+            256,
+            256,
+            6
+        );
+        this.load.audio("meaw", [
+            "assets/audio/thunder.mp3",
+            "assets/audio/thunder.ogg"
+        ]);
         this.load.image("arrow", "assets/images/arrow.png");
     }, // End preload
     create: function() {
@@ -47,11 +75,16 @@ var GameState = {
             char = self.characters.create(
                 -1000,
                 this.game.world.centerY,
-                element.key
+                element.key,
+                0
             );
-            char.customParams = { text: element.text };
+            char.customParams = {
+                text: element.text,
+                sound: self.game.add.audio("meaw")
+            };
             char.anchor.setTo(0.5);
 
+            char.animations.add("animate", [0, 1, 2, 3], 4, false);
             char.inputEnabled = true;
             char.input.pixelPerfectClick = true;
             char.events.onInputDown.add(self.animateChar, self);
@@ -62,7 +95,6 @@ var GameState = {
             this.game.world.centerX,
             this.game.world.centerY
         );
-
         //right arrow button(sprite)
         this.rightArrow = this.game.add.sprite(
             580,
@@ -97,26 +129,56 @@ var GameState = {
     }, // End create
     update: function() {}, // End update
     switchChar: function(sprite, event) {
+        // check if the animation is still running
+        // if true prevent the function from running
+        if (this.isMoving) return false;
+        this.isMoving = true;
+
         var newChar, endX;
         if (sprite.customParams.direction == 1) {
+            // set the new character to the next one
             newChar = this.characters.next();
+            // make the new character come from the left side
+            newChar.x = -this.currentChar.width / 2;
+            // set the x position of the current character to the right
             endX = 640 + this.currentChar.width / 2;
+            this.animateChar(this.currentChar);
         } else {
+            // set the new characte to the previous one
             newChar = this.characters.previous();
+            // make the new character come from the right side
+            newChar.x = 640 + this.currentChar.width / 2;
+            // set the x position of the current character to the left
             endX = -this.currentChar.width / 2;
         }
-        this.currentChar.x = endX;
+        // smooth transition of the new character to the center of the screen
+        var newCharTween = this.game.add.tween(newChar);
+        newCharTween.to({ x: this.game.world.centerX }, 1000);
+        newCharTween.start();
+        // smooth transition of the current character to the
+        // left or right of the screen depending on the value of endX
+        var currentCharTween = this.game.add.tween(this.currentChar);
+        currentCharTween.to({ x: endX }, 1000);
+        // set this.isMoving to false when the animation is complete
+        currentCharTween.onComplete.add(function() {
+            this.isMoving = false;
+            isLooping = true;
+        }, this);
+        currentCharTween.start();
+        // set the current character to the new one
         this.currentChar = newChar;
-        this.currentChar.x = this.game.world.centerX;
+        this.animateChar(newChar);
     },
     animateChar: function(sprite, event) {
-        sprite.tint = rgb(255, 0, 100);
+        sprite.animations.play("animate");
+        sprite.customParams.sound.play();
     }
 }; // End GameState
-var game = new Phaser.Game(640, 360, Phaser.AUTO);
-game.state.add("GameState", GameState);
-game.state.start("GameState");
-
+// create the game
+var game = new Phaser.Game(640, 360, Phaser.AUTO); // Phaser.AUTO means choose the renderer automatically
+game.state.add("GameState", GameState); // add the game state
+game.state.start("GameState"); // start the game
+//convert rgb to hex
 function rgb(r, g, b) {
     return (r << 16) | (g << 8) | b;
 }

37b2c0edb438f02bc5de26e2bd1f71b850858d9b
Mohammed Salman <msal4@outlook.com>
2017-09-21 22:01:48 +0300

    g

--- super-mario/main.js
+++ super-mario/main.js
@@ -1,13 +1,31 @@
-var game = new Phaser.Game(300, 300, Phaser.AUTO, '', {preload:preload, create:create, update:update});
+var game = new Phaser.Game(500, 400, Phaser.AUTO, "", {
+    preload: preload,
+    create: create,
+    update: update
+});
 
 function preload() {
-
+    game.load.spritesheet(
+        "marioSmall",
+        "assets/images/marioSmall.png",
+        34,
+        34,
+        6,
+        1
+    );
+    game.load.image("ground", "assets/images/ground.png");
 }
 
+var mario;
+var ground;
 function create() {
-
+    ground = game.add.sprite(0, 370, "ground");
+    game.physics.arcade.enable(ground);
+    ground.body.immovable = true;
+    mario = new Mario(100, 100, 700, 1000, 100);
 }
 
 function update() {
-    
+    mario.checkInput();
+    game.physics.arcade.collide(ground, mario.sprite);
 }
